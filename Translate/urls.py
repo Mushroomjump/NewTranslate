0e85adf77198b7d70f4c68a4c8ceb106cc70c432
Mushroomjump <38023334+Mushroomjump@users.noreply.github.com>
2024-04-02 14:24:36 +0300

    Update urls.py

--- Translate/urls.py
+++ Translate/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='templates/index'),
+    path('', views.index, name='Translate/templates/index'),
     path('navigate/', views.navigate, name='navigate'),
     path('translate/', views.translate, name='translate'),
 ]
